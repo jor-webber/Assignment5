7e13a53dc65238edfc8b509b9ee3ef2cfc91f2cd
jordan webber <webber_jordan_95@hotmail.com>
2022-04-07 19:34:45 -0400

    Completed problem 2

---
+++ Problem2/CPU.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem2
+{
+    /// <summary>
+    /// Represents a cpu
+    /// </summary>
+    public class Cpu
+    {
+        private double _speed;
+        private string _manufacturer;
+        private int _numberOfCores;
+
+        /// <summary>
+        /// Gets and sets the speed
+        /// </summary>
+        public double Speed { 
+            get => _speed;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Speed cannot be 0 or lower");
+                }
+                _speed = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the manufacturer
+        /// </summary>
+        public string Manufacturer { 
+            get => _manufacturer;
+            set
+            {
+                if (!value.ToUpper().Equals("INTEL") || !value.ToUpper().Equals("AMD"))
+                {
+                    throw new ArgumentException("Manufacturer must be either Intel and AMD");
+                }
+                _manufacturer = value.ToUpper();
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the socket type
+        /// </summary>
+        public string SocketType { get; set; }
+
+        /// <summary>
+        /// Gets and sets the cache size
+        /// </summary>
+        public double CacheSize { get; set; }
+
+        /// <summary>
+        /// Gets and sets the number of cores
+        /// </summary>
+        public int NumberOfCores { 
+            get => _numberOfCores;
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Number of cores cannot be less than 1");
+                }
+                _numberOfCores = value;
+            }
+        }
+    }
+}
---
+++ Problem2/Memory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Problem2
+{
+    /// <summary>
+    /// Represents memory
+    /// </summary>
+    public class Memory
+    {
+        private double _readSpeed;
+        private double _writeSpeed;
+        private string _type;
+
+        /// <summary>
+        /// Gets and sets the read speed
+        /// </summary>
+        public double ReadSpeed
+        {
+            get => _readSpeed;
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException($"Read speed cannot be less than 1");
+                }
+
+                _readSpeed = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets write speed
+        /// </summary>
+        public double WriteSpeed
+        {
+            get => _writeSpeed;
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException($"Write speed cannot be less than 1");
+                }
+                _writeSpeed = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the type
+        /// </summary>
+        [MaxLength(4)]
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                if (!value.ToUpper().Equals("DDR1") || !value.ToUpper().Equals("DDR2") ||
+                    !value.ToUpper().Equals("DDR3") || !value.ToUpper().Equals("DDR4"))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Type), "Memory type must be DDR1, DDR2, DDR3, or DDR4");
+                }
+                _type = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the amount in GB
+        /// </summary>
+        public double AmountInGb { get; set; }
+    }
+}
