019c7d94ccf278294c6ee8d7737757756912c302
harman-d97 <77417939+harman-d97@users.noreply.github.com>
2022-04-08 16:59:48 -0400

    unsubscriber class for problem 4

---
+++ Problem4/Unsubscriber.cs
@@ -0,0 +1,35 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem4
+{
+    public class Unsubscriber : IDisposable
+    {
+        private List<IObserver<T>> observers;
+        private IObserver<T> observer;
+
+        /// <summary>
+        /// Create a new instance of a unsubscriber
+        /// </summary>
+        /// <param name="observers"></param>
+        /// <param name="observer"></param>
+        internal Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
+        {
+            this.observers = observers;
+            this.observer = observer;
+        }
+
+        /// <summary>
+        /// Removes an observer 
+        /// </summary>
+        public void Dispose()
+        {
+            if(this.observers.Contains(this.observer))
+            {
+                this.observers.Remove(this.observer);
+            }
+        }
+    }
+}
